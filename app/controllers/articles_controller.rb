b56aa4571165b098946727a7f28b70d4557ae16a
Jake <jrdriscoll17@gmail.com>
2020-12-13 04:07:45 -0600

    Updated article index page with proper formatting and added properly formatted test data to prevent wasted api calls

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = fetch_articles
+    @articles = fetch_articles_testing
   end
   
   def new; end
@@ -15,8 +15,40 @@ class ArticlesController < ApplicationController
 
   def fetch_articles
     response = HTTP.headers(:'X-Api-Key' => "#{ENV["NEWS_API_KEY"]}").get("https://newsapi.org/v2/top-headlines?country=us")
-
+    # binding.pry
     return nil if response.status != 200
     JSON.parse(response)['articles']
+  end
+
+  def fetch_articles_testing
+    response = [
+      {
+        "source"=>{"id"=>nil, "name"=>"YouTube"}, 
+        "author"=>nil,
+        "title"=>"Florida defender throws cleat in wild ending vs. LSU | 2020 College Football Highlights - ESPN","description"=>"With two minutes to go in a tied game, Florida's Marco Wilson gets hit with an unsportsmanlike conduct penalty as he throws the cleat of LSU’s Kole Taylor ac...",
+        "url"=>"https://www.youtube.com/watch?v=Fif3Rkl5_nQ",
+        "urlToImage"=>"https://i.ytimg.com/vi/Fif3Rkl5_nQ/maxresdefault.jpg",
+        "publishedAt"=>"2020-12-13T06:11:10Z",
+        "content"=>nil
+      },
+      {
+        "source"=>{"id"=>nil, "name"=>"YouTube"}, 
+        "author"=>nil,
+        "title"=>"Florida defender throws cleat in wild ending vs. LSU | 2020 College Football Highlights - ESPN","description"=>"With two minutes to go in a tied game, Florida's Marco Wilson gets hit with an unsportsmanlike conduct penalty as he throws the cleat of LSU’s Kole Taylor ac...",
+        "url"=>"https://www.youtube.com/watch?v=Fif3Rkl5_nQ",
+        "urlToImage"=>"https://i.ytimg.com/vi/Fif3Rkl5_nQ/maxresdefault.jpg",
+        "publishedAt"=>"2020-12-13T06:11:10Z",
+        "content"=>nil
+      },
+      {
+        "source"=>{"id"=>nil, "name"=>"YouTube"}, 
+        "author"=>nil,
+        "title"=>"Florida defender throws cleat in wild ending vs. LSU | 2020 College Football Highlights - ESPN","description"=>"With two minutes to go in a tied game, Florida's Marco Wilson gets hit with an unsportsmanlike conduct penalty as he throws the cleat of LSU’s Kole Taylor ac...",
+        "url"=>"https://www.youtube.com/watch?v=Fif3Rkl5_nQ",
+        "urlToImage"=>"https://i.ytimg.com/vi/Fif3Rkl5_nQ/maxresdefault.jpg",
+        "publishedAt"=>"2020-12-13T06:11:10Z",
+        "content"=>nil
+      }
+    ]
   end
 end
